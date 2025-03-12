e93b5226c3d068417dfc637f81201b9fec21fd26
devops-tausifalam <83172943+devops-tausifalam@users.noreply.github.com>
2025-03-12 17:53:49 +0530

    STAGE 1 (completed)
    completed basic skeleton as per the core design along with some tweaks from the original design to maintain schematic design. STAGE 2: is about making (this) working through initial menifest or meta.json structure, later on to procceed with plug-in developmenetlike for youtube, spotify and carousel etc.

---
+++ src/components/share.jsx
@@ -0,0 +1,15 @@
+import "./styles/share.css";
+import { MdOutlineIosShare } from "react-icons/md";
+
+export default function Share() {
+    return (
+        <>
+        <div className="sharebtn">
+          <button>
+            <MdOutlineIosShare className="icon"/>
+          </button>
+        </div>
+        <div className="sharemenu"></div>
+      </>
+    );
+}
--- src/hook.jsx
+++ src/hook.jsx
@@ -1,18 +1,18 @@
-import Brand from './components/branding.jsx'
-import Userprofile from './components/user_profile.jsx'
-import Socialcapsule  from './components/capsule.jsx'
-import Linklist from './components/link.jsx'    
-
+import Brand from "./components/branding.jsx";
+import Userprofile from "./components/user_profile.jsx";
+import Socialcapsule from "./components/capsule.jsx";
+import Linklist from "./components/link.jsx";
+import Share from "./components/share.jsx";
 function App() {
-
   return (
-    <div className='mainWrapper'>
-     <Userprofile/>
-     <Linklist/>
-     <Socialcapsule/>
-     <Brand/>
+    <div className="mainWrapper">
+      <Share/>
+      <Userprofile />
+      <Linklist />
+      <Socialcapsule />
+      <Brand />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
