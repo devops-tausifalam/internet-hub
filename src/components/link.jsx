9c5ca8ac4635ef76095d8ee29f3d0754b3e7311a
devops-tausifalam <83172943+devops-tausifalam@users.noreply.github.com>
2025-03-17 12:59:19 +0530

    added share menu in links

--- src/components/link.jsx
+++ src/components/link.jsx
@@ -1,8 +1,34 @@
 import "./styles/link.scss";
+import { useState } from "react";
+import { RiFlag2Line } from "react-icons/ri";
+import { IoMdShareAlt } from "react-icons/io";
 import { BsThreeDotsVertical } from "react-icons/bs";
 import meta from "./meta/hook.metadata.json" with { type: "json" };
 
 export default function Linklist() {
+  const [activeMenu, setActiveMenu] = useState(null);
+
+  const toggleMenu = (index) => (event) => {
+    event.stopPropagation(); // prevent click from bubbling up
+
+    if (activeMenu === index) {
+      setActiveMenu(null);
+    } else {
+      setActiveMenu(index)
+    
+
+    // attach event listener to detect outside click
+    const handleOutsideClick = (e) => {
+      if (!e.target.closest(".hrefsymbol")) {
+        setActiveMenu(null);
+        document.removeEventListener("click", handleOutsideClick);
+      };
+    };
+      document.addEventListener("click", handleOutsideClick);
+    }
+  };
+
+
   return (
     <div className="linkbox">
       <ul className="link-wrapper">
@@ -10,7 +36,21 @@ export default function Linklist() {
           <li className="link" key={index}>
             <a href={link.url} target="_blank">{link.title}</a>
             <div className="hrefsymbol">
-              <BsThreeDotsVertical className="icon" />
+              <BsThreeDotsVertical className="icon" onClick={toggleMenu(index)}/>
+              <div className={`shareto-menu ${activeMenu === index ? "active" : ""}`}>
+                <ul>
+                  <li>
+                    <button>
+                      <IoMdShareAlt /> share
+                    </button>
+                  </li>
+                  <li>
+                    <button>
+                      <RiFlag2Line />  report
+                    </button>
+                    </li>
+                </ul>
+              </div>
             </div>
           </li>
         ))}
