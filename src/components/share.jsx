f24bb14c4500768dd03460f77d1cb2793bcd60bb
devops-tausifalam <83172943+devops-tausifalam@users.noreply.github.com>
2025-03-23 15:18:15 +0530

    design ready for share menu

--- src/components/share.jsx
+++ src/components/share.jsx
@@ -1,8 +1,24 @@
 import "./styles/share.scss";
-import { MdOutlineIosShare, MdOutlineClose, MdInsertLink, MdApps    } from "react-icons/md";
-import {brandingForPages as Hook} from "./branding.jsx"
+import { RiFlag2Line } from "react-icons/ri";
+import { MdOutlineIosShare, MdOutlineClose } from "react-icons/md";
+// common social icons
+import { FaSnapchatGhost } from "react-icons/fa";
+import {
+  FaSquareFacebook,
+  FaFacebookMessenger,
+  FaWhatsapp,
+  FaSignalMessenger,
+  FaLinkedin,
+  FaBluesky,
+  FaXTwitter,
+  FaEnvelope,
+} from "react-icons/fa6";
 
-export function Share({onClick}) {
+// metadata for profile and user names and other details if required
+import meta from "./meta/hook.metadata.json" with { type: "json" };
+
+
+export function Share({ onClick }) {
   return (
     <>
       <div className="sharebtn">
@@ -14,46 +30,76 @@ export function Share({onClick}) {
   );
 }
 
-export function ShareMenu({onClose}) {
+export function ShareMenu({ onClose }) {
   return (
     <>
       <div className="sharemenu">
         <div className="boxmodal">
-            <div className="controls">
-                <h2>
-                    Share hook.
-                </h2>
-                <button onClick={onClose}>
-                  <MdOutlineClose className="icon"/>
+          <div className="controls">
+            <h2>Share hook.</h2>
+            <button onClick={onClose}>
+              <MdOutlineClose className="icon" />
+            </button>
+          </div>
+          <figure>
+            <div className="avatar">
+              <img src={meta.profile.avatar} alt="user-avatar" />
+            </div>
+            <div className="user-name">
+              <p>
+                {meta.profile.name}
+              </p>
+              <span>{meta.profile.bio}</span>
+            </div>
+          </figure>
+          <div className="functionBox">
+              <div className="socialicons">
+                <button>
+                  <FaSquareFacebook className="icon" />
+                  <span className="icon-label">Facebook</span>
                 </button>
-            </div>
-            <figure>
-              <div className="avatar">
-
+                <button>
+                  <FaFacebookMessenger className="icon" />
+                  <span className="icon-label">Messenger</span>
+                </button>
+                <button>
+                  <FaWhatsapp className="icon" />
+                  <span className="icon-label">WhatsApp</span>
+                </button>
+                <button>
+                  <FaSignalMessenger className="icon" />
+                  <span className="icon-label">Signal</span>
+                </button>
+                <button>
+                  <FaLinkedin className="icon" />
+                  <span className="icon-label">LinkedIn</span>
+                </button>
+                <button>
+                  <FaSnapchatGhost className="icon" />
+                  <span className="icon-label">Snapchat</span>
+                </button>
+                <button>
+                  <FaBluesky className="icon" />
+                  <span className="icon-label">Bluesky</span>
+                </button>
+                <button>
+                  <FaXTwitter className="icon" />
+                  <span className="icon-label">X</span>
+                </button>
+                <button>
+                  <FaEnvelope className="icon" />
+                  <span className="icon-label">Email</span>
+                </button>
+                <button className="primarySharebtn">
+                  <span className="icon-label">more</span>
+                </button>
               </div>
-              <div className="user-name">
-                  <p>Tausif Alam</p>
-              </div>
-            </figure>
-            <div className="functionBox">
-              <section className="grid1">
-                <div className="pageLinkbtn">
-                  <button>
-                    <MdInsertLink className="icon"/> 
-                     copy hook
-                  </button>
-                </div>
-                <div className="primarySharebtn">
-                  <button>
-                  <MdApps className="icon"/>
-                  </button>
-                </div>
-              </section>
-              <section className="grid2">
-                <div className="socialicons"></div>
-              </section>
-            </div>
-            <div className="reportsection"></div>
+          </div>
+          <div className="reportsection">
+            <button>
+                <RiFlag2Line /> Report this hook.
+            </button>
+          </div>
         </div>
       </div>
     </>
